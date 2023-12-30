b571b2db9c84af9480ac1f6d54ba93aabf1cf6e8
krishanleong <krishanleong@gmail.com>
2023-12-29 23:51:07 -0500

    Updated display timings

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import { electiveData } from "./utils/electives";
 
 function App() {
-  const [grade, setGrade] = useState("5th");
+  const [grade, setGrade] = useState("");
   const [math7th, setMath7th] = useState("");
   const [electives, setElectives] = useState(electiveData);
   const [step, setStep] = useState(1);
@@ -44,6 +44,14 @@ function App() {
     setElectives(updatedElectives);
   };
 
+  function handleReset(e) {
+    e.preventDefault();
+    setGrade("");
+    setMath7th("");
+    setStep(1);
+    setElectives(electiveData);
+  }
+
   function handleLock(e) {
     e.preventDefault();
     if (step === 1) {
@@ -55,7 +63,7 @@ function App() {
     if (step === 2) {
       const updatedElectives = electives.map((elective) =>
         elective.check && !elective.chosen
-          ? { ...elective, chosen: true }
+          ? { ...elective, firstAlt: true }
           : elective
       );
       setElectives(updatedElectives);
@@ -63,7 +71,7 @@ function App() {
     if (step === 3) {
       const updatedElectives = electives.map((elective) =>
         elective.check && !elective.chosen
-          ? { ...elective, chosen: true }
+          ? { ...elective, secondAlt: true }
           : elective
       );
       setElectives(updatedElectives);
@@ -160,140 +168,145 @@ function App() {
                   7th Grade History
                 </label>
               </div>
-              <div className="Electives">
-                <h3 style={{ display: "inline" }}>
-                  {step === 1 && "Select your Electives"}
-                  {step === 2 && "Select your first Alternate Electives"}
-                  {step === 3 && "Select your second Alternate Electives"}
-                  {step === 4 && "Elective Selection Complete"}
-                </h3>
-                {step !== 4 && (
-                  <button onClick={(e) => handleLock(e)}>Lock in</button>
-                )}
-                <div className="electiveContainer">
-                  {electives.map((elective, index) => {
-                    return (
-                      <div className="electivechoice">
-                        <input
-                          type="checkbox"
-                          id={elective.name}
-                          name={elective.name}
-                          key={elective.name}
-                          checked={elective.check}
-                          onChange={() => handleOnChange(index)}
-                          disabled={
-                            (currElectives + elective.length > maxElectives &&
-                              elective.check === false) ||
-                            elective.chosen === true ||
-                            step === 4
-                          }
-                        />
-                        <label
-                          htmlFor={elective.name}
-                          className={
-                            elective.length === 0.5
-                              ? "checklabel half"
-                              : "checklabel full"
-                          }
-                        >
-                          {elective.name} -{" "}
-                          {elective.length === 0.5 ? "semester" : "full year"}
-                        </label>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-              <div className="Schedule">
-                <div className="column">
-                  Block
-                  <div className="class">1st block</div>
-                  <div className="class">2nd block</div>
-                  <div className="class">3rd block</div>
-                  <div className="class">4th block</div>
-                </div>
-                <div className="column">
-                  A Day
-                  <div className="core class">Language Arts</div>
-                  <div className="full class">Health/PE</div>
-                  <div className="core class">{math7th}</div>
-                  <div className="core class">Science</div>
-                </div>
-                <div className="column">
-                  B Day
-                  <div className="core class">Language Arts</div>
-                  {/* Elective block start ***************************** */}
-                  {electiveChoices.length === 0 && (
-                    <div className="class">elective block</div>
+              {math7th !== "" && (
+                <div className="Electives">
+                  <h3 style={{ display: "inline" }}>
+                    {step === 1 && "Select your Electives"}
+                    {step === 2 && "Select your first Alternate Electives"}
+                    {step === 3 && "Select your second Alternate Electives"}
+                    {step === 4 && "Elective Selection Complete"}
+                  </h3>
+                  {step !== 4 && (
+                    <button onClick={(e) => handleLock(e)}>Lock in</button>
                   )}
-                  {semElectiveChoices.length === 1 && (
-                    <>
-                      <div className="class half">
-                        {electiveChoices[0].name}
-                      </div>
-                      <div className="halfclass">elective block</div>
-                    </>
-                  )}
-                  {semElectiveChoices.length >= 2 && (
-                    <>
-                      <div className="class half">
-                        {electiveChoices[0].name}
-                      </div>
-                      <div className="class half">
-                        {electiveChoices[1].name}
-                      </div>
-                    </>
-                  )}
-                  {yearElectiveChoices.length >= 1 &&
-                    semElectiveChoices.length === 0 && (
-                      <div className="class full">
-                        {electiveChoices[0].name}
-                      </div>
-                    )}
-                  {math7th === "7thmath" ? (
+                  <div className="electiveContainer">
+                    {electives.map((elective, index) => {
+                      return (
+                        <div className="electivechoice">
+                          <input
+                            type="checkbox"
+                            id={elective.name}
+                            name={elective.name}
+                            key={elective.name}
+                            checked={elective.check}
+                            onChange={() => handleOnChange(index)}
+                            disabled={
+                              (currElectives + elective.length > maxElectives &&
+                                elective.check === false) ||
+                              elective.chosen === true ||
+                              elective.firstAlt === true ||
+                              step === 4
+                            }
+                          />
+                          <label
+                            htmlFor={elective.name}
+                            className={
+                              elective.length === 0.5
+                                ? "checklabel half"
+                                : "checklabel full"
+                            }
+                          >
+                            {elective.name} -{" "}
+                            {elective.length === 0.5 ? "semester" : "full year"}
+                          </label>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+              {math7th !== "" && (
+                <div className="Schedule">
+                  <div className="column">
+                    Block
+                    <div className="class">1st block</div>
+                    <div className="class">2nd block</div>
+                    <div className="class">3rd block</div>
+                    <div className="class">4th block</div>
+                  </div>
+                  <div className="column">
+                    A Day
+                    <div className="core class">Language Arts</div>
+                    <div className="full class">Health/PE</div>
                     <div className="core class">{math7th}</div>
-                  ) : (
-                    <>
-                      {semElectiveChoices.length +
-                        yearElectiveChoices.length * 2 <
-                        3 && <div className="class">elective block</div>}
-                      {semElectiveChoices.length === 3 && (
-                        <>
-                          <div className="class half">
-                            {electiveChoices[2].name}
-                          </div>
-                          <div className="halfclass">elective block</div>
-                        </>
-                      )}
-                      {semElectiveChoices.length === 4 && (
-                        <>
-                          <div className="class half">
-                            {electiveChoices[2].name}
-                          </div>
-                          <div className="class half">
-                            {electiveChoices[3].name}
-                          </div>
-                        </>
-                      )}
-                      {yearElectiveChoices.length === 1 &&
-                        semElectiveChoices.length <= 2 &&
-                        semElectiveChoices.length !== 0 && (
-                          <div className="class full">
-                            {yearElectiveChoices[0].name}
-                          </div>
-                        )}
-
-                      {yearElectiveChoices.length === 2 && (
+                    <div className="core class">Science</div>
+                  </div>
+                  <div className="column">
+                    B Day
+                    <div className="core class">Language Arts</div>
+                    {/* Elective block start ***************************** */}
+                    {electiveChoices.length === 0 && (
+                      <div className="class">elective block</div>
+                    )}
+                    {semElectiveChoices.length === 1 && (
+                      <>
+                        <div className="class half">
+                          {electiveChoices[0].name}
+                        </div>
+                        <div className="halfclass">elective block</div>
+                      </>
+                    )}
+                    {semElectiveChoices.length >= 2 && (
+                      <>
+                        <div className="class half">
+                          {electiveChoices[0].name}
+                        </div>
+                        <div className="class half">
+                          {electiveChoices[1].name}
+                        </div>
+                      </>
+                    )}
+                    {yearElectiveChoices.length >= 1 &&
+                      semElectiveChoices.length === 0 && (
                         <div className="class full">
-                          {yearElectiveChoices[1].name}
+                          {electiveChoices[0].name}
                         </div>
                       )}
-                    </>
-                  )}
-                  <div className="core class">History</div>
+                    {math7th === "7thmath" ? (
+                      <div className="core class">{math7th}</div>
+                    ) : (
+                      <>
+                        {semElectiveChoices.length +
+                          yearElectiveChoices.length * 2 <
+                          3 && <div className="class">elective block</div>}
+                        {semElectiveChoices.length === 3 && (
+                          <>
+                            <div className="class half">
+                              {electiveChoices[2].name}
+                            </div>
+                            <div className="halfclass">elective block</div>
+                          </>
+                        )}
+                        {semElectiveChoices.length === 4 && (
+                          <>
+                            <div className="class half">
+                              {electiveChoices[2].name}
+                            </div>
+                            <div className="class half">
+                              {electiveChoices[3].name}
+                            </div>
+                          </>
+                        )}
+                        {yearElectiveChoices.length === 1 &&
+                          semElectiveChoices.length <= 2 &&
+                          semElectiveChoices.length !== 0 && (
+                            <div className="class full">
+                              {yearElectiveChoices[0].name}
+                            </div>
+                          )}
+
+                        {yearElectiveChoices.length === 2 && (
+                          <div className="class full">
+                            {yearElectiveChoices[1].name}
+                          </div>
+                        )}
+                      </>
+                    )}
+                    <div className="core class">History</div>
+                  </div>
+                  <div className="column">Alternates</div>
                 </div>
-                <div className="column">Alternates</div>
-              </div>
+              )}
             </>
           )}
           {grade === "7th" && (
@@ -302,7 +315,12 @@ function App() {
             </>
           )}
         </div>
-        <button>Submit</button>
+        {grade !== "" && (
+          <>
+            <button disabled={step !== 4}>Submit</button>
+            <button onClick={(e) => handleReset(e)}>Reset Everything</button>
+          </>
+        )}
       </form>
     </div>
   );
